7394db9c24b3df2158b44aee351b4b8772ea0bb4
Alex Mandel <tech@wildintellect.com>
2022-01-19 11:02:55 -0800

    Chasing linter

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -8,7 +8,7 @@ from stactools.palsar.commands import create_palsar_command
 
 
 class CommandsTest(CliTestCase):
-    
+
     def create_subcommand_functions(self):
         return [create_palsar_command]
 
