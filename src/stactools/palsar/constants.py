5b54253c3bab37be98e16ae20c0a5366e1821655
Alex Mandel <tech@wildintellect.com>
2022-01-20 13:17:39 -0800

    Adding constants

---
+++ src/stactools/palsar/constants.py
@@ -0,0 +1,18 @@
+from pystac import Provider, Link
+
+ALOS_PALSAR_PLATFORM = "alos-2/alos"
+ALOS_PALSAR_INSTRUMENTS = ["PALSAR-2", "PALSAR"]
+ALOS_PALSAR_GSD = 25  # meters
+ALOS_PALSAR_EPSG = 4326
+ALOS_PALSAR_PROVIDERS = [
+    Provider("Japan Aerospace Exploration Agency",
+             roles=["producer", "processor", "licensor"],
+             url="https://www.eorc.jaxa.jp/ALOS/en/dataset/fnf_e.htm"),
+]
+ALOS_PALSAR_LINKS = [
+    Link("handbook",
+         "https://www.eorc.jaxa.jp/ALOS/en/dataset/pdf/DatasetDescription_PALSAR2_Mosaic_FNF_revK.pdf",
+         "application/pdf",
+         "Global 25 m Resolution PALSAR-2/PALSAR Mosaic and Forest/Non-Forest Map (FNF) Dataset Description",
+         extra_fields={"description": "Also includes data usage information"})
+]
