82dae224a622226d8207002099d698f857b5de73
Alex Bush <alexbush@developmentseed.org>
2022-01-26 18:28:05 +0000

    feat: add function for conversion

--- src/stactools/palsar/cog.py
+++ src/stactools/palsar/cog.py
@@ -2,6 +2,9 @@ import logging
 import os
 import subprocess
 
+from rio_cogeo.cogeo import cog_translate
+from rio_cogeo.profiles import cog_profiles
+
 from stactools.palsar.errors import CogifyError
 from stactools.palsar.utils import extract_archive, palsar_folder_parse
 
@@ -36,24 +39,39 @@ def cogify(tile_path: str, output_directory: str):
         outfile = os.path.join(output_directory, cog_name)
         infile = os.path.join(directory, variable)
 
-        args = []
-        args.append("gdal_translate")
-        args.extend([
-            "-of",
-            "COG",
-            "-co",
-            "compress=deflate",
-        ])
-        args.extend([infile, outfile])
+        # args = []
+        # args.append("gdal_translate")
+        # args.extend([
+        #     "-of",
+        #     "COG",
+        #     "-co",
+        #     "compress=deflate",
+        # ])
+        # args.extend([infile, outfile])
 
-        logger.info(f"Running {args}")
-        result = subprocess.run(args, capture_output=True)
-        logger.info(result.stdout.decode('utf-8').strip())
-        if result.returncode != 0:
-            logger.error(result.stderr.decode('utf-8').strip())
-            raise CogifyError(result.stderr.decode('utf-8').strip())
-        else:
-            logger.info(result.stderr.decode('utf-8').strip())
+        # logger.info(f"Running {args}")
+        # result = subprocess.run(args, capture_output=True)
+
+        output_profile = cog_profiles.get("deflate")
+        output_profile.update(dict(BIGTIFF="IF_SAFER"))
+        output_profile.update({})
+
+        # Dataset Open option (see gdalwarp `-oo` option)
+        config = dict(
+            GDAL_NUM_THREADS="ALL_CPUS",
+            GDAL_TIFF_INTERNAL_MASK=True,
+            GDAL_TIFF_OVR_BLOCKSIZE="128",
+        )
+
+        cog_translate(
+            infile,
+            outfile,
+            output_profile,
+            config=config,
+            in_memory=False,
+            quiet=True,
+        )
+        logging.info("Wrote out to " + outfile)
         cogs[variable] = outfile
 
     # return list of cogs by band
