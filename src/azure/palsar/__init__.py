5893517b41e4cbdd59fe74b6e59ec3f805afad80
Alex Bush <alexbush@developmentseed.org>
2022-02-24 20:04:48 +0000

    fix: linting

--- src/azure/palsar/__init__.py
+++ src/azure/palsar/__init__.py
@@ -7,7 +7,8 @@ from azure.storage.blob import BlobServiceClient  # type: ignore
 
 from stactools.palsar import cog, stac
 
-blob_service_client = BlobServiceClient.from_connection_string(os.environ["AzureWebJobsStorage"])
+blob_service_client = BlobServiceClient.from_connection_string(
+    os.environ["AzureWebJobsStorage"])
 
 def main(msg: func.QueueMessage) -> None:
     start_time = time.time()
@@ -44,7 +45,8 @@ def main(msg: func.QueueMessage) -> None:
         logging.info('Saved COGs at' + str(cogs))
         for cogfile in list(cogs.values()):
             _, tail = os.path.split(cogfile)
-            blob_client = blob_service_client.get_blob_client(container=output_container, blob=rootdir + '/' + tail)
+            blob_client = blob_service_client.get_blob_client(
+                container=output_container, blob=rootdir + '/' + tail)
             with open(cogfile, "rb") as data:
                 try:
                     blob_client.upload_blob(data, overwrite=True)
@@ -52,7 +54,7 @@ def main(msg: func.QueueMessage) -> None:
                 except Exception as e:
                     logging.info(f"Exception {e} for {cogfile}")
 
-        base_url=os.path.dirname(blob_client.url)
+        base_url = os.path.dirname(blob_client.url)
         item = stac.create_item(cogs, base_url)
         json_file = '_'.join((os.path.basename(filename)).split("_")[0:3])
         json_path = os.path.join('/tmp', f'{json_file}.json')
@@ -64,7 +66,8 @@ def main(msg: func.QueueMessage) -> None:
         logging.info("Saved STAC JSON at " + json_path)
 
         _, tail = os.path.split(json_path)
-        blob_client = blob_service_client.get_blob_client(container=output_container, blob=rootdir + '/' + tail)
+        blob_client = blob_service_client.get_blob_client(
+            container=output_container, blob=rootdir + '/' + tail)
         with open(json_path, "rb") as data:
             try:
                 blob_client.upload_blob(data, overwrite=True)
