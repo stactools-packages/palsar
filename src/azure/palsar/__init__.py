9e707064154cdc3db4ab237f176bfc59697ebce5
Alex Bush <alexbush@developmentseed.org>
2022-02-24 18:31:59 +0000

    feat: WIP add STAC to function

--- src/azure/palsar/__init__.py
+++ src/azure/palsar/__init__.py
@@ -5,8 +5,21 @@ import time
 import azure.functions as func  # type: ignore
 from azure.storage.blob import BlobServiceClient  # type: ignore
 
-from stactools.palsar import cog
+from stactools.palsar import cog, stac
 
+blob_service_client = BlobServiceClient.from_connection_string(os.environ["AzureWebJobsStorage"])
+
+def process_cogfile(rootdir, cogfile) -> None:
+    _, tail = os.path.split(cogfile)
+    blob_client = blob_service_client.get_blob_client(
+        container="output", blob=rootdir + '/' + tail)
+    # Upload the created file
+    with open(cogfile, "rb") as data:
+        try:
+            blob_client.upload_blob(data, overwrite=True)
+            logging.info("Success for " + cogfile + "@" + rootdir + tail)
+        except Exception as e:
+            logging.info(f"Exception {e} for {cogfile}")
 
 def main(msg: func.QueueMessage) -> None:
     start_time = time.time()
@@ -17,34 +30,47 @@ def main(msg: func.QueueMessage) -> None:
         filename = body
     logging.info('Python queue trigger function processed a queue item: %s',
                  body)
-    blob_service_client = BlobServiceClient.from_connection_string(
-        os.environ["AzureWebJobsStorage"])
+    rootdir, _ = os.path.split(filename)
     blob_client = blob_service_client.get_blob_client(container="dltest",
                                                       blob=filename)
-    dir, _ = os.path.split(filename)
     if blob_client.exists():
         bd = blob_client.download_blob()
 
-        path, file = os.path.split(filename)
-        target_path = '/tmp/' + file
-        with open(target_path, 'wb') as target_file:
+        _, file = os.path.split(filename)
+        input_targz_filepath = '/tmp/' + file
+        with open(input_targz_filepath, 'wb') as target_file:
             bd.readinto(target_file)
-        logging.info('Saved input at ' + target_path)
-        cogs = cog.cogify(target_path, '/tmp')
+        logging.info('Saved input at ' + input_targz_filepath)
+        
+        cogs = cog.cogify(input_targz_filepath, '/tmp')
+        
         logging.info('Saved COGs at' + str(cogs))
         for cogfile in list(cogs.values()):
-            _, tail = os.path.split(cogfile)
-            blob_client = blob_service_client.get_blob_client(
-                container="output", blob=dir + '/' + tail)
-            # Upload the created file
-            temp_path = cogfile
-            with open(temp_path, "rb") as data:
-                try:
-                    blob_client.upload_blob(data, overwrite=True)
-                    logging.info("Success for " + temp_path + "@" + dir + tail)
-                except Exception as e:
-                    logging.info(f"Exception {e} for {temp_path}")
-                os.remove(temp_path)
+            process_cogfile(rootdir, cogfile)
+
+        url="http://google.no"
+        item = stac.create_item(cogs, url) 
+        json_file = '_'.join((os.path.basename(filename)).split("_")[0:3]) 
+        json_path = os.path.join('/tmp', f'{json_file}.json') 
+        item.set_self_href(os.path.join(url, os.path.basename(json_path))) 
+        # TODO: gracefully fail if validate doesn't work 
+        item.validate() 
+        item.save_object(dest_href=json_path)
+
+        logging.info("Saved STAC JSON at " + json_path)
+
+        _, tail = os.path.split(json_path)
+        blob_client = blob_service_client.get_blob_client(container="output", blob=rootdir + '/' + tail)
+        with open(json_path, "rb") as data:
+            try:
+                blob_client.upload_blob(data, overwrite=True)
+                logging.info("Successfully uploaded STAC JSON for " + filename)
+            except Exception as e:
+                logging.info(f"Exception {e} for {json_path}")
+
+        for cogfile in list(cogs.values()):
+            os.remove(cogfile)
+            logging.info("Cleaned up " + cogfile)
 
         end_time = time.time()
         logging.info(f"Runtime is {end_time - start_time}")
